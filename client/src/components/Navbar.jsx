cf4b56f7f2a25dbabd8473a66cab86efb731f003
th-neri <nerithiago123@gmail.com>
2025-08-28 17:22:39 -0600

    second commit. added more code lines to appcontext, started and finished the product details part.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
 
     const [open, setOpen] = React.useState(false)
-    const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery } = useAppContext();
+    const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery, getCartCount, getCartAmount } = useAppContext();
 
     const logout = async () => {
         setUser(null);
@@ -38,7 +38,7 @@ const Navbar = () => {
 
                 <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
                 {!user ? (<button onClick={() => setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
@@ -55,10 +55,16 @@ const Navbar = () => {
                     )}
             </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
-                {/* Menu Icon SVG */}
-                <img src={assets.menu_icon} alt="menu" />
-            </button>
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
+                    {/* Menu Icon SVG */}
+                    <img src={assets.menu_icon} alt="menu" />
+                </button>
+            </div>
 
             {/* Mobile Menu */}
             {open && (
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -60,8 +60,32 @@ export const AppContextProvider = ({ children }) => {
         toast.success("Removed from Cart")
         setCartItems(cartData)
     }
-    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin,
-         products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery }
+
+    //get cart item count
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const item in cartItems) {
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    }
+
+    //get total cart amount
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id == items);
+            if (cartItems[items] > 0) {
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    }
+
+    const value = {
+        navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin,
+        products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery, getCartCount, getCartAmount
+    }
 
     return <AppContext.Provider value={value}>
         {children}
