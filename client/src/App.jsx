9488808c964fae9290901d728924dbc1fc7fa3f9
th-neri <nerithiago123@gmail.com>
2025-09-01 19:30:29 -0600

    fourth commit. finished My Orders page and started the Seller Layout page where i am currently finishing Orders part.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,19 +11,25 @@ import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
+import MyOrders from './pages/MyOrders';
+import SellerLogin from './components/seller/SellerLogin';
+import SellerLayout from './pages/seller/SellerLayout';
+import AddProduct from './pages/seller/AddProduct';
+import ProductList from './pages/seller/ProductList';
+import Orders from './pages/seller/Orders';
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext()
+  const { showUserLogin, isSeller } = useAppContext()
 
   return (
-    <div>
-      { isSellerPath ? null : <Navbar />}
-      { showUserLogin ? <Login /> : null}
-      
+    <div className='bg-white text-gray-700 text-default min-h-screen'>
+      {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
+
       <Toaster />
-    <div className={`${isSellerPath ? "" : "py-6 md:px-16 lg:px-24 xl:px-32"}`}>
+      <div className={`${isSellerPath ? "" : "py-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<AllProducts />} />
@@ -31,6 +37,12 @@ const App = () => {
           <Route path='/products/:category/:id' element={<ProductDetails />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/add-address' element={<AddAddress />} />
+          <Route path='/my-orders' element={<MyOrders />} />
+          <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path='product-list' element={<ProductList />} />
+            <Route path='orders' element={<Orders />} />
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
