91c8cba8b238d9d3d1bf7c4ff95d2c65d9aea830
th-neri <nerithiago123@gmail.com>
2025-11-18 18:48:47 -0600

    eighth commit. currently coding the back-end and finishing the payment method part.

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -59,6 +59,19 @@ const Cart = () => {
                 } else {
                     toast.error(data.message)
                 }
+            } else {
+                //Place order with Stripe
+                const { data } = await axios.post('/api/order/stripe', {
+                    userId: user._id,
+                    items: cartArray.map(item => ({ product: item._id, quantity: item.quantity })),
+                    address: selectAddress._id
+                })
+
+                if (data.success) {
+                    window.location.replace(data.url)
+                } else {
+                    toast.error(data.message)
+                }
             }
         } catch (error) {
             toast.error(error.message)
